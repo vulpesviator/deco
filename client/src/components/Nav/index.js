deb682b704b3e7e7e927035038ea1efebd537c59
Danny Wittig <wittigdaniel.g@gmail.com>
2023-09-06 21:51:12 -0400

    cleaned up client folder

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -9,11 +9,6 @@ function Nav() {
       return (
         <ul className="flex-row">
           <li className="mx-1">
-            <Link to="/orderHistory">
-              Order History
-            </Link>
-          </li>
-          <li className="mx-1">
             {/* this is not using the Link component to logout or user and then refresh the application to the start */}
             <a href="/" onClick={() => Auth.logout()}>
               Logout
@@ -43,8 +38,7 @@ function Nav() {
     <header className="flex-row px-1">
       <h1>
         <Link to="/">
-          <span role="img" aria-label="shopping bag">🛍️</span>
-          -Shop-Shop
+          Deco
         </Link>
       </h1>
 
