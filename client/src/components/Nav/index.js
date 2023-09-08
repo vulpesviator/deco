75be8ec9b8d9ed1be5bb19136bfe348e122035cf
travdoeswhat <123843930+vulpesviator@users.noreply.github.com>
2023-09-07 21:25:50 -0500

    Added nav links

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,26 +4,21 @@ import { Link } from "react-router-dom";
 import { Button, Menu, Dropdown } from "semantic-ui-react";
 
 function Nav() {
-
   function showNavigation() {
     if (Auth.loggedIn()) {
       return (
         <Menu.Menu position="right">
-          <Menu.Item onClick={() => Auth.logout()}>
-            Logout
-          </Menu.Item>
+          <Menu.Item onClick={() => Auth.logout()}>Logout</Menu.Item>
         </Menu.Menu>
       );
     } else {
       return (
         <Menu.Menu position="right">
           <Menu.Item>
-            <Link to="/quiz">
-              Find Your Style
-            </Link>
+            <Link to="/quiz">Find Your Style</Link>
           </Menu.Item>
           <Menu.Item>
-          <Dropdown text="Browse Art">
+            <Dropdown text="Browse Art">
               <Dropdown.Menu>
                 <Dropdown.Item as={Link} to="/browse">
                   All Art
@@ -38,19 +33,17 @@ function Nav() {
             </Dropdown>
           </Menu.Item>
           <Menu.Item>
-            <Link to="/signup">
-              Signup
+            <Link to="/signup">Signup</Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link to="/login">
+              <Button className="primary">Log In</Button>
             </Link>
           </Menu.Item>
           <Menu.Item>
-            <Button className="primary" to="/test">
-              Log In
-            </Button>
-          </Menu.Item>
-          <Menu.Item>
-            <Button className="secondary text-dark" to="/test">
-              Take the Test
-            </Button>
+            <Link to="/test">
+              <Button className="secondary text-dark">Take the Test</Button>
+            </Link>
           </Menu.Item>
         </Menu.Menu>
       );
@@ -60,9 +53,7 @@ function Nav() {
   return (
     <Menu secondary>
       <Menu.Item>
-        <Link to="/">
-          Deco
-        </Link>
+        <Link to="/">Deco</Link>
       </Menu.Item>
       {showNavigation()}
     </Menu>
