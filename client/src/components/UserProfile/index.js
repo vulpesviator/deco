e4f817d5c6d8961a8d97caebdc3aa7c3d212ea51
Danny Wittig <wittigdaniel.g@gmail.com>
2023-09-07 20:24:02 -0400

    profile page
    routed and component

---
+++ client/src/components/UserProfile/index.js
@@ -0,0 +1,11 @@
+
+function UserProfile() {
+
+  return (
+    <div>
+      <h2>This is a profile</h2>
+    </div>   
+  );
+  }
+  
+export default UserProfile;
---
+++ client/src/pages/Profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+import UserProfile from '../components/UserProfile'
+
+const ProfilePage = () => {
+  return (
+    <div className="container">
+      <UserProfile />
+    </div>
+  );
+};
+
+export default ProfilePage;
