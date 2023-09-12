b73d618bdcfa4c3012531c993762a73563548869
travdoeswhat <123843930+vulpesviator@users.noreply.github.com>
2023-09-11 20:49:49 -0500

    Added semantic native image carousel

---
+++ client/src/pages/Art.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Container, Header, Message, Segment } from 'semantic-ui-react';
+import "pure-react-carousel/dist/react-carousel.es.css";
+import ImageCarousel from '../components/ImageCarousel';
+
+function Art() {
+    return (
+        <Container style={{ margin: 20 }}>
+          <Segment attached="top">
+            <Header as="h2" content="Image carousel" />
+            <p>
+              This prototype features how to create a carousel with the{" "}
+              <code>Image</code> component, take a look into{" "}
+              <code>examples/ImageCarousel</code> to get more details.
+            </p>
+          </Segment>
+    
+          <Segment attached="bottom">
+            <ImageCarousel />
+          </Segment>
+        </Container>
+      );
+}
+
+export default Art;
