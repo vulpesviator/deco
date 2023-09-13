48bdbd66fad5b92165f4644864397e192f0cf375
travdoeswhat <123843930+vulpesviator@users.noreply.github.com>
2023-09-12 19:03:56 -0500

    Added slick carousel

--- client/src/components/ImageCarousel/index.js
+++ client/src/components/ImageCarousel/index.js
@@ -1,28 +1,66 @@
 import React from "react";
+import Slider from 'react-slick';
+import { Container } from 'semantic-ui-react';
 
-import ImageGallery from "react-image-gallery";
-import "react-image-gallery/styles/css/image-gallery.css";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-const images = [
-  {
-    original: "https://picsum.photos/id/1018/1000/600/",
-    thumbnail: "https://picsum.photos/id/1018/250/150/",
-  },
-  {
-    original: "https://picsum.photos/id/1015/1000/600/",
-    thumbnail: "https://picsum.photos/id/1015/250/150/",
-  },
-  {
-    original: "https://picsum.photos/id/1019/1000/600/",
-    thumbnail: "https://picsum.photos/id/1019/250/150/",
-  },
-];
+const ImageCarousel = () => {
+  const settings = {
+    centerMode: true,
+    centerPadding: '60px',
+    lazyLoad: 'ondemand',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
 
-class ImageCarousel extends React.Component {
-    render() {
-      return <ImageGallery items={images} />;
-    }
-  }
+  return (
+    <Container>
+      <Slider {...settings}>
+        <div>
+          <img src="https://picsum.photos/id/1018/1000/600/" />
+        </div>
+        <div>
+          <img src="https://picsum.photos/id/1015/1000/600/" />
+        </div>
+        <div>
+          <img src="https://picsum.photos/id/1019/1000/600/" />
+        </div>
+        <div>
+          <img src="https://picsum.photos/id/1019/1000/600/" />
+        </div>
+        <div>
+          <img src="https://picsum.photos/id/1019/1000/600/" />
+        </div>
+        <div>
+          <img src="https://picsum.photos/id/1019/1000/600/" />
+        </div>
+      </Slider>
+    </Container>
+  );
+}
 
 
 export default ImageCarousel;
