4252b66103d06c2d2c51776825648b6d4c05ca49
clarebryar <128009509+clarebryar@users.noreply.github.com>
2023-09-14 15:57:44 -0500

    removed unused items

--- client/src/components/Question/index.js
+++ client/src/components/Question/index.js
@@ -1,91 +1,82 @@
+import React, { useEffect, useState } from "react";
+import {
+  Container,
+  Grid,
+  Header,
+  Radio,
+  Rating,
+  Button,
+} from "semantic-ui-react";
+import { useQuery } from "@apollo/client";
+import { QUERY_IMAGES } from "../../utils/queries";
+import { Routes, useNavigate, Navigate } from "react-router-dom";
 
-import React, { useEffect, useState } from 'react';
-import { Container, Grid, Header, Radio, Rating, Button } from 'semantic-ui-react';
-import { useQuery } from '@apollo/client';
-import { QUERY_IMAGES } from '../../utils/queries';
-import {Routes, useNavigate, Navigate} from 'react-router-dom';
-
-
-export 
-
-function Question() {
-  
+export function Question() {
   const [activeQuestion, setActiveQuestion] = useState(0);
-  
+
   const [isDisabled, setDisabled] = useState(false);
   const { loading, data } = useQuery(QUERY_IMAGES);
   const images = data?.images || [];
-  const currentImage = images[activeQuestion]
-  console.log(images)
-  console.log(currentImage)
-  
+  const currentImage = images[activeQuestion];
+  console.log(images);
+  console.log(currentImage);
+
   function RatingSlider() {
     const [rating, setRating] = useState(1);
-    const [userRating, setUserRating] = useState();
-    const ratingArray = []
+    console.log(
+      "category:",
+      currentImage.category.name,
+      "category ID:",
+      currentImage.category._id,
+      "rating:",
+      rating
+    );
     const handleChange = (e) => {
       setRating(e.target.value);
-      const userInput = e.target.value
-      ratingArray.push(userInput)
-      console.log('category:', currentImage.category.name, 'category ID:', currentImage.category._id, 'rating', userRating, )
-      // console.log('userRating STate:', userRating)
-      setUserRating(userInput)
     };
 
-    console.log(userRating)
-   
-  if (activeQuestion === 17){
-  setDisabled(true)
-  }
+    if (activeQuestion === 17) {
+      setDisabled(true);
+    }
 
-  const navigate = useNavigate();
-  const navigateProfile = () => {
-    navigate('/profile');
-  };
+    const navigate = useNavigate();
+    const navigateProfile = () => {
+      navigate("/profile");
+    };
     const onClickNext = (event) => {
-      
       event.preventDefault();
-  
       event.stopPropagation();
-      if (activeQuestion !== images.length - 1) { setActiveQuestion((prev) => prev + 1)
-       
-     } else if (isDisabled){
-       navigateProfile()
-     }
-     }
-  
-      return (
-        <>
+      if (activeQuestion !== images.length - 1) {
+        setActiveQuestion((prev) => prev + 1);
+      } else if (isDisabled) {
+        navigateProfile();
+      }
+    };
+
+    return (
+      <>
         <div>
           <Grid.Row>
-          <div>Rating: {rating}</div>
-          <input
-            type='range'
-            min={1}
-            max={5}
-            value={rating}
-            onChange={handleChange}
-          /><br />
-          <Rating
-            icon="star"
-            maxRating={5}
-            rating={rating}
-          />
+            <div>Rating: {rating}</div>
+            <input
+              type="range"
+              min={1}
+              max={5}
+              value={rating}
+              onChange={handleChange}
+            />
+            <br />
+            <Rating icon="star" maxRating={5} rating={rating} />
           </Grid.Row>
         </div>
-       <Grid.Row>
-        <Button primary onClick={onClickNext} >
-           { isDisabled? 'See Your Results' : 'Next'}
+        <Grid.Row>
+          <Button primary onClick={onClickNext}>
+            {isDisabled ? "See Your Results" : "Next"}
           </Button>
-          </Grid.Row>
+        </Grid.Row>
       </>
-      )
+    );
   }
-
- 
-  
-   
-
 
   return (
     <Container>
@@ -93,24 +84,28 @@ function Question() {
         <div>Loading...</div>
       ) : (
         <div>
+          <Header as="h3" textAlign="center">
+            <p> Rate the following Artworks</p>
+          </Header>
 
-      <Header as='h3' textAlign='center'>
-       <p> Rate the following Artworks</p>
-      </Header>
-
-         <Grid textAlign='center'>
-       
-        <Grid.Row> <img src={currentImage.src} alt={currentImage.src} height = '200' width = '200' margin = '10px' padding = '10px'></img>
-         </Grid.Row>
-                
-                  <RatingSlider />
-               
-                 
-                  <Grid.Row key='temp'>
-            
+          <Grid textAlign="center">
+            <Grid.Row>
+              {" "}
+              <img
+                src={currentImage.src}
+                alt={currentImage.src}
+                height="200"
+                width="200"
+                margin="10px"
+                padding="10px"
+              ></img>
             </Grid.Row>
-                  </Grid>  
-             </div>
+
+            <RatingSlider />
+
+            <Grid.Row key="temp"></Grid.Row>
+          </Grid>
+        </div>
       )}
     </Container>
   );
